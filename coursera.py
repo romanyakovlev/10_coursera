893ba82a93770b6026976de3b16b7acee1487e6c
Roman <yakovlevroman8@gmail.com>
2016-11-01 18:50:39 +0700

    Update coursera.py

--- coursera.py
+++ coursera.py
@@ -11,7 +11,7 @@ import json
 def get_courses_list():
     xml_page = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
     root = etree.fromstring(xml_page.content)
-    random_courses = [course[0].text for course in random.sample(set(root), 20)]
+    random_courses = [course[0].text for course in random.sample(set(root), 2)]
     return random_courses
 
 
@@ -28,12 +28,12 @@ def get_course_info(course_slug):
         course_slug + '&fields=plannedLaunchDate').text)['elements'][0]['plannedLaunchDate']
         course_start_date = date_json
     try:
-        course_rating = soup.find(class_='ratings-text').text
+        course_rating = soup.find(class_ = 'ratings-text').text
     except:
         course_rating = 'No rating'
-    course_name = soup.find(class_='course-name-text').text
-    course_language = soup.find(class_='language-info').text
-    weeks_list = soup.find_all(class_='week')
+    course_name = soup.find(class_ = 'course-name-text').text
+    course_language = soup.find(class_ = 'language-info').text
+    weeks_list = soup.find_all(class_ = 'week')
     course_weeks_amount = len(weeks_list) if weeks_list else 'No information'
     return course_name, course_rating, course_language, course_start_date, course_weeks_amount
 
@@ -41,15 +41,19 @@ def get_course_info(course_slug):
 def output_courses_info_to_xlsx(filepath):
     wb = Workbook()
     ws = wb.active
-    for column, value in enumerate(['Name', 'Rating', 'Language', 'Start Date','Weeks Amount']):
-        _ = ws.cell(column=column+1, row=1, value="{}".format(value))
+    for column_counter, value in enumerate(['Name', 'Rating', 'Language', 'Start Date','Weeks Amount']):
+        column_counter += 1
+        _ = ws.cell(column = column_counter, row = 1, value = value)
     coursers_list = get_courses_list()
     for course_counter, course in enumerate(coursers_list):
+        course_counter += 1
+        row_counter = course_counter + 1
         course_slug = course.split('/')[-1]
         course_values = get_course_info(course_slug)
-        for column, value in enumerate(course_values):
-            _ = ws.cell(column=column+1, row=course_counter+2, value="{}".format(value))
-        print(' {0}/{1} courses parsed'.format(course_counter+1, len(coursers_list)), end='\r')
+        for column_counter, value in enumerate(course_values):
+            column_counter += 1
+            _ = ws.cell(column = column_counter, row = row_counter , value = str(value))
+        print('{0}/{1} courses parsed'.format(course_counter, len(coursers_list)), end='\r')
     wb.save(filepath)
 
 

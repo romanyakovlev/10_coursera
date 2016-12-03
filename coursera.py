7abf565b9eeec7da741e4067e52e0fedd1fce803
Roman Yakovlev <yakovlevroman8@gmail.com>
2016-12-04 00:32:03 +0700

    Update coursera.py

--- coursera.py
+++ coursera.py
@@ -9,51 +9,57 @@ import json
 
 
 def get_courses_list():
+    courses_amount = 3
     xml_page = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
-    root = etree.fromstring(xml_page.content)
-    random_courses = [course[0].text for course in random.sample(set(root), 20)]
+    xml_root = etree.fromstring(xml_page.content)
+    random_courses = [course[0].text for course in random.sample(set(xml_root), courses_amount)]
     return random_courses
 
 
 def get_course_info(course_slug):
     full_path = 'https://www.coursera.org/learn/{}'.format(course_slug)
     html_doc = requests.get(full_path).content
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    soup_data = BeautifulSoup(html_doc, 'html.parser')
     try:
-        request_params = {"q" : "slug", "slug" : course_slug, "fields" : "upcomingSessionStartDate"}
+        request_params = {"q": "slug", "slug": course_slug, "fields": "upcomingSessionStartDate"}
         date_json = json.loads(requests.get("https://api.coursera.org/api/courses.v1/",
-            params = request_params).text)['elements'][0]['upcomingSessionStartDate']
+            params=request_params).text)['elements'][0]['upcomingSessionStartDate']
         course_start_date = date.fromtimestamp(date_json / 1000.0)
     except KeyError:
-        request_params = {"q" : "slug", "slug" : course_slug, "fields" : "plannedLaunchDate"}
+        request_params = {"q": "slug", "slug": course_slug, "fields": "plannedLaunchDate"}
         date_json = json.loads(requests.get("https://api.coursera.org/api/courses.v1/",
-            params = request_params).text)['elements'][0]['plannedLaunchDate']
+            params=request_params).text)['elements'][0]['plannedLaunchDate']
         course_start_date = date_json
     try:
-        course_rating = soup.find(class_ = 'ratings-text').text
+        course_rating = soup_data.find(class_='ratings-text').text
     except AttributeError:
         course_rating = 'No rating'
-    course_name = soup.find(class_ = 'course-name-text').text
-    course_language = soup.find(class_ = 'language-info').text
-    weeks_list = soup.find_all(class_ = 'week')
+    course_name = soup_data.find(class_='course-name-text').text
+    course_language = soup_data.find(class_='language-info').text
+    weeks_list = soup_data.find_all(class_='week')
     course_weeks_amount = len(weeks_list) if weeks_list else 'No information'
     return course_name, course_rating, course_language, course_start_date, course_weeks_amount
 
 
 def output_courses_info_to_xlsx(filepath):
-    wb = Workbook()
-    ws = wb.active
-    arr = iter(['Name', 'Rating', 'Language', 'Start Date','Weeks Amount'])
-    for cells in ws.iter_rows(max_row=1,max_col=5,min_row=1):
+    workbook = Workbook()
+    worksheet = workbook.active
+    fields_name = iter(['Name', 'Rating', 'Language', 'Start Date','Weeks Amount'])
+    min_row_num, max_row_num, max_col_num  = 1, 1, 5
+    for cells in worksheet.iter_rows(max_row=max_row_num,
+                                                        max_col=max_col_num, min_row=min_row_num):
         for cell in cells:
-            cell.value = next(arr)
+            cell.value = next(fields_name)
     coursers_list = get_courses_list()
-    for cells, course in zip(ws.iter_rows(max_row=21,max_col=5,min_row=2), coursers_list):
+    second_row, last_row = 2, 21
+    for cells, course in zip(
+            worksheet.iter_rows(max_row=last_row, max_col=max_col_num, min_row=second_row),
+            coursers_list):
         course_slug = course.split('/')[-1]
-        params = get_course_info(course_slug)
-        for cell, param in zip(cells, params):
+        course_params = get_course_info(course_slug)
+        for cell, param in zip(cells, course_params):
             cell.value = param
-    wb.save(filepath)
+    workbook.save(filepath)
 
 
 if __name__ == '__main__':

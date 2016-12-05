824823570914ba7d1aa90827e9acca6d0c4c1bdc
Roman Yakovlev <yakovlevroman8@gmail.com>
2016-12-05 23:45:40 +0700

    Update coursera.py

--- coursera.py
+++ coursera.py
@@ -20,18 +20,20 @@ def get_course_info(course_slug):
     html_doc = requests.get(full_path).content
     soup_data = BeautifulSoup(html_doc, 'html.parser')
     request_params = {"q": "slug", "slug": course_slug,
-                      "fields": "plannedLaunchDate,upcomingSessionStartDate"}
+                                   "fields": "plannedLaunchDate,upcomingSessionStartDate"}
     response_json = json.loads(requests.get("https://api.coursera.org/api/courses.v1/",
-                                            params=request_params).text)
+                                                                     params=request_params).text)
     session_dates = response_json['elements'][0]
     try:
         timestamp_in_millisecs = session_dates['upcomingSessionStartDate']
+    except KeyError:
+        session_dates["upcomingSessionStartDate"] = None
+    if session_dates["upcomingSessionStartDate"]:
         millisecs_in_seconds = 1000.0
         timestamp_in_secs = timestamp_in_millisecs / millisecs_in_seconds
         course_start_date = date.fromtimestamp(timestamp_in_secs)
-    except KeyError:
-        date_from_json = session_dates['plannedLaunchDate']
-        course_start_date = date_from_json
+    else:
+        course_start_date = session_dates['plannedLaunchDate']
     try:
         course_rating = soup_data.find(class_='ratings-text').text
     except AttributeError:
